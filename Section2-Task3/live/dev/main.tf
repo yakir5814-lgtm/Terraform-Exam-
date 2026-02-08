928c9079e253f64beb9557411878610e38bce26e
yakir <yakir5814@gmail.com>
2026-02-08 14:12:20 +0200

    Remove comment about task value definitions
    Removed comment in Hebrew about defining values for the task.

--- Section2-Task3/live/dev/main.tf
+++ Section2-Task3/live/dev/main.tf
@@ -1,7 +1,7 @@
 module "my_infrastructure" {
   source = "../../modules/network_app"
 
-  # כאן אנחנו מגדירים את הערכים לפי המשימה
+ 
   vpc_cidr         = "10.0.0.0/16"
   subnet_count     = 2
   instance_type    = "t3.micro"
